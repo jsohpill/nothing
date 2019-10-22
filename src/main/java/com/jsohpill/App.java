5d92d5b43b45195ce2ca102e13e4b975832503fd
chuandadexiaoyu <yuyanlin201268@gmail.com>
2019-10-22 19:19:49 +0800

    测试oop的继承

---
+++ src/main/java/com/jsohpill/App.java
@@ -0,0 +1,16 @@
+package com.jsohpill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+
+
+        Child child = new Child("Mike");
+    }
+}
