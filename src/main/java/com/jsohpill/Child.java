caa32e38a0c9444b44d8d34126946167271e6250
chuandadexiaoyu <yuyanlin201268@gmail.com>
2019-10-22 19:19:31 +0800

    测试oop的继承

---
+++ src/main/java/com/jsohpill/Child.java
@@ -0,0 +1,12 @@
+package com.jsohpill;
+
+public class Child extends Parent {
+
+    Child() {
+        System.out.println("I am in Child()");
+    }
+
+    Child(String name) {
+        System.out.println("I am in Child(String name)");
+    }
+}
---
+++ src/main/java/com/jsohpill/Parent.java
@@ -0,0 +1,14 @@
+package com.jsohpill;
+
+public class Parent {
+
+    public String name;
+
+    Parent() {
+        System.out.println("I am in Parent()");
+    }
+
+    Parent(String name) {
+        System.out.println("I am in Parent(String name)");
+    }
+}
